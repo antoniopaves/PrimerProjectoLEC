e9c1c6ba7f499530af190269023603897ae79dc1
antoniopaves <antoniopaves877@gmail.com>
2025-07-21 14:58:04 -0400

    Primeros cambios

---
+++ src/main/java/cl/lec/InterfazEstudiante.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cl.lec;
+
+/**
+ *
+ * @author 22775980-1
+ */
+public class InterfazEstudiante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
