e84cb3c4ff214ad8eb77547582d1f61f98a6d546
antoniopaves <antoniopaves877@gmail.com>
2025-07-21 15:16:29 -0400

    Interfaz grafica solo visual

--- src/main/java/cl/lec/PrimerProjectoLEC.java
+++ src/main/java/cl/lec/PrimerProjectoLEC.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package cl.lec;
 
 /**
  *
  * @author 22775980-1
  */
-public class InterfazEstudiante {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class PrimerProjectoLEC {
+    
 }
